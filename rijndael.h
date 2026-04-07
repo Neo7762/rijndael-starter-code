721f522752c0b777a3d359b240eadfe16b555160
Neo7762 <leahmarieayoub@live.ie>
2026-04-07 12:19:30 +0000

    Added the code explanations at the top of the Rijndael C and H files

--- rijndael.h
+++ rijndael.h
@@ -1,19 +1,17 @@
 /*
- * TODO: Add your name and student number here, along with
- *       a brief description of this code.
+ * Leah Marie Ayoub - X00217593
+ * This code is the public interface for the Rijndael.c file
+ * It tells users what functions are available, what they take as input, what
+ * they return, and what data types exist. It is very similar to a modern day
+ * API documentation, but in code form.
  */
 
 #ifndef RIJNDAEL_H
 #define RIJNDAEL_H
 
-typedef enum {
-  AES_BLOCK_128,
-  AES_BLOCK_256,
-  AES_BLOCK_512
-} aes_block_size_t;
+typedef enum { AES_BLOCK_128, AES_BLOCK_256, AES_BLOCK_512 } aes_block_size_t;
 
-unsigned char block_access(unsigned char *block,
-                           size_t row, size_t col,
+unsigned char block_access(unsigned char *block, size_t row, size_t col,
                            aes_block_size_t block_size);
 
 /*
@@ -21,13 +19,9 @@ unsigned char block_access(unsigned char *block,
  * entry point to the library for programmes hoping to use it to
  * encrypt or decrypt data)
  */
-unsigned char *aes_encrypt_block(
-    unsigned char *plaintext,
-    unsigned char *key,
-    aes_block_size_t block_size);
-unsigned char *aes_decrypt_block(
-    unsigned char *ciphertext,
-    unsigned char *key,
-    aes_block_size_t block_size);
+unsigned char *aes_encrypt_block(unsigned char *plaintext, unsigned char *key,
+                                 aes_block_size_t block_size);
+unsigned char *aes_decrypt_block(unsigned char *ciphertext, unsigned char *key,
+                                 aes_block_size_t block_size);
 
 #endif
